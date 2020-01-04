cd144cb05007cd402d7cf606f1a6f3e34e0ee059
yy <mengyy_linux@163.com>
2020-01-04 16:20:05 +0800

    custom tensorboard callback

---
+++ senti_analysis/callbacks.py
@@ -0,0 +1,14 @@
+# encoding: utf8
+
+import logging
+import tensorflow as tf
+
+_logger = logging.getLogger()
+
+
+class CustomTensorBoard(tf.keras.callbacks.TensorBoard):
+    def on_train_batch_end(self, batch, logs=None):
+        _logger.info('train batch {} end'.format(batch))
+        logs = logs or {}
+        self._log_metrics(logs, prefix='', step=batch)
+        super(CustomTensorBoard, self).on_train_batch_end(batch, logs)
