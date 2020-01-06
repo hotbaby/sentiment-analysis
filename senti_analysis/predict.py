80079e48e382e8e65a20f3c2f4d406c35b4fd4a8
yy <mengyy_linux@163.com>
2020-01-06 16:39:07 +0800

    add predict module
    Signed-off-by: yy <mengyy_linux@163.com>

---
+++ senti_analysis/predict.py
@@ -0,0 +1,37 @@
+# encoding: utf8
+
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from senti_analysis import config
+from senti_analysis.models.model_v1 import get_model
+from senti_analysis.preprocess import encode_sentence, text2sentence
+
+
+def predict(text_list:list):
+    inverse_label_map = {
+        0: -2,
+        1: -1,
+        2: 0,
+        3: 1
+    }
+
+    model = get_model()
+    model.load_weights(os.path.join(config.MODEL_PATH, 'model_v1.h5'))
+
+    sentences = text2sentence(text_list)
+    encoded_sentences = encode_sentence(sentences, padding=True, max_length=config.MAX_SEQUENCE_LENGTH)
+
+    predictions = model.predict(encoded_sentences)
+
+    pred_results = []
+    for pred in predictions:
+        pred_result = []
+        for label in pred:
+            transform_label = inverse_label_map[np.argmax(label)]
+            pred_result.append(transform_label)
+        pred_results.append(pred_result)
+
+    return pd.DataFrame(np.array(pred_results).T, columns=model.output_names)
